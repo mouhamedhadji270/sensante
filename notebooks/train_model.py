798c91502ab053f3e1a4cf0e820d9ab211bc219f
Mouhamed Hadji <mouhamedhadji270@gmail.com>
2026-05-10 22:26:30 +0000

    Lab 3 : API FastAPI avec /health et /predict (v2)

--- notebooks/train_model.py
+++ notebooks/train_model.py
@@ -139,3 +139,43 @@ print(f"\nProbabilites par classe :")
 for classe, proba in zip(model_loaded.classes_, probas):
     bar = '#' * int(proba * 30)
     print(f" {classe:8s} : {proba:.1%} {bar}")
+importances = model.feature_importances_
+for name, imp in sorted(zip(feature_cols, importances),
+key=lambda x: x[1], reverse=True):print(f" {name:20s} : {imp:.3f}")
+patients = [
+    # 1. Jeune sans symptômes
+    {
+        'age': 20, 'sexe': 'M', 'temperature': 36.8,
+        'tension_sys': 120, 'toux': False,
+        'fatigue': False, 'maux_tete': False,
+        'region': 'Dakar'
+    },
+
+    # 2. Adulte avec forte fièvre
+    {
+        'age': 35, 'sexe': 'F', 'temperature': 40.0,
+        'tension_sys': 115, 'toux': True,
+        'fatigue': True, 'maux_tete': True,
+        'region': 'Dakar'
+    },
+
+    # 3. Patient âgé avec toux
+    {
+        'age': 70, 'sexe': 'M', 'temperature': 37.8,
+        'tension_sys': 130, 'toux': True,
+        'fatigue': True, 'maux_tete': False,
+        'region': 'Dakar'
+    }
+]
+for p in patients:
+    sexe_enc = le_sexe_loaded.transform([p['sexe']])[0]
+    region_enc = le_region_loaded.transform([p['region']])[0]
+
+    features = [[
+        p['age'], sexe_enc, p['temperature'], p['tension_sys'],
+        int(p['toux']), int(p['fatigue']), int(p['maux_tete']),
+        region_enc
+    ]]
+
+    pred = model_loaded.predict(features)[0]
+    print(f"Patient {p['age']} ans → Diagnostic : {pred}")
